9856f6be85cab17f7f813ab0ebf7e841a4c84dd4
unknown <marcosemilio99@gmail.com>
2025-07-19 20:13:34 -0600

    desktop version

--- background.py
+++ background.py
@@ -1,4 +1,14 @@
 import keyboard
+import ctypes
+import os
 
-backgroundpath = 'C:\Users\KSEQUEIRA\AppData\Roaming\Microsoft\Windows\Themes\TranscodedWallpaper'
+
+
+image_path = r"C:\Users\USER\Pictures\programtest\mary.png"
+
+
+
+for files in os.path.join(r"C:\Users\USER\Pictures\programtest", ):
+    keyboard.add_hotkey(". + ,")
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, files, 1 | 2)
 
